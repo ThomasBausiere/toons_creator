373eac146967f5b9d656270a256a82dbe1f210f9
Thomas <t.bausiere@gmail.com>
2025-06-18 11:10:11 +0200

    first commit

---
+++ src/main/java/org/example/toons/ToonsApplication.java
@@ -0,0 +1,13 @@
+package org.example.toons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToonsApplication.class, args);
+    }
+
+}
