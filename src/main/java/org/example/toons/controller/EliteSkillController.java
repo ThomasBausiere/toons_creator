46073d2a6e336ea2d2996357aa5d5d99d3a73826
Thomas <t.bausiere@gmail.com>
2025-06-19 15:28:32 +0200

    Read update delete Controller

--- src/main/java/org/example/toons/controller/EliteSkillController.java
+++ src/main/java/org/example/toons/controller/EliteSkillController.java
@@ -5,9 +5,10 @@ import org.example.toons.service.EliteSkillService;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 public class EliteSkillController {
 
@@ -43,15 +44,52 @@ public class EliteSkillController {
     //READ
 
     //detail
-
+    @GetMapping("/skill-detail/{skillId}")
+    public String getEliteSkillById(Model model, @PathVariable("skillId")UUID skillId){
+        model.addAttribute("skill", eliteSkillService.getEliteSkillById(skillId));
+        return "skill-detail";
+    }
     //list
+    @GetMapping("/skill-list")
+    public String getAllSkill(Model model){
+        List<EliteSkill> eliteSkills = eliteSkillService.getAllEliteSkills();
+        model.addAttribute("eliteSkills", eliteSkills);
+        return "skill-list";
+    }
 
     //UPDATE
 
+        //vers le form d'update
+        @GetMapping("/skill-update/{skillId}")
+        public String updateSkill(Model model, @PathVariable("skillId") UUID skillId){
+        model.addAttribute("skill", eliteSkillService.getEliteSkillById(skillId));
+        return "skill";
+        }
+
+        //récuperation et validation du form de modification
+        @PutMapping("/add-skill")
+        public String submiteSkillChange(@Validated @ModelAttribute("skill") EliteSkill eliteSkill, BindingResult bindingResult ){
+            if(bindingResult.hasErrors()){
+                System.out.println("erreur lors de l'update de la compétence");
+                return "redirect:/skill-list";
+            }else {
+                eliteSkillService.updateEliteSkillName(
+                        eliteSkill.getId(),
+                        eliteSkill.getName(),
+                        eliteSkill.getDescription()
+                );
+                return "redirect:/skill-list";
+            }
+        }
 
     //add_boss
 
     //remove_boss
 
     //DELETE
+    @GetMapping("/delete/{skillId}")
+    public String deleteEliteSkill(@PathVariable("skillId") UUID skillId){
+        eliteSkillService.deleteEliteSkill(skillId);
+        return "redirect:/list";
+    }
 }
